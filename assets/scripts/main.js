dc067322d5eb9b4b6b819b0cbd24fd84549f7bf2
= <sinulinggatwo@gmail.com>
2021-02-21 00:32:34 +0700

    fixing the bug of button prev and next slider image

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,6 +1,11 @@
 var slideIndex = 0;
 showSlides();
 
+function plusSlides(n) {
+  showSlideN(slideIndex += n);
+}
+
+
 function showSlides() {
   var i;
   var slides = document.getElementsByClassName("my-slides");
@@ -11,4 +16,16 @@ function showSlides() {
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 5000); // Change image every 2 seconds
+}
+
+
+function showSlideN(n) {
+  var i;
+  var slides = document.getElementsByClassName("my-slides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
 }
